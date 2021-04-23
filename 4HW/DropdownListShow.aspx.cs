bad2a753018d3d8c31173c1200bbb3330e89e118
aa7898 <107111228@mail.oit.edu.tw>
2021-04-23 22:11:35 +0800

    作業4

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -8,7 +8,28 @@ using System.Web.UI.WebControls;
 namespace _4HW {
     public partial class DropdownListShow : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            ddl_Area.Items.Add("北區");
+            ddl_Area.Items.Add("中區");
+            ddl_Place.Items.Add("基隆");
+            ddl_Place.Items.Add("台北");
+            ddl_Place.Items.Add("新北");
+        }
 
+        protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddl_Place.Items.Clear();
+            if (ddl_Area.SelectedValue == "北區")
+            {
+                ddl_Place.Items.Add("基隆");
+                ddl_Place.Items.Add("台北");
+                ddl_Place.Items.Add("新北");
+            }
+            else if (ddl_Area.SelectedValue == "中區")
+            {
+                ddl_Place.Items.Add("苗栗");
+                ddl_Place.Items.Add("台中");
+                ddl_Place.Items.Add("南投");
+            }
         }
     }
 }
